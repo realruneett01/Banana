25778e432daf0e90acf09759d445840eae18678e
realruneett <realruneett@gmail.com>
2026-07-02 21:53:10 +0530

    feat: Add custom-kicad theme and configure custom layer colors as default

--- src/kicanvas/themes/index.ts
+++ src/kicanvas/themes/index.ts
@@ -1,14 +1,15 @@
 /*
-    Copyright (c) 2023 Alethea Katherine Flowers.
-    Published under the standard MIT License.
-    Full text available at: https://opensource.org/licenses/MIT
+ Copyright (c) 2023 Alethea Katherine Flowers.
+ Published under the standard MIT License.
+ Full text available at: https://opensource.org/licenses/MIT
 */
 
 import { type Theme } from "../../kicad";
 import witch_hazel from "./witch-hazel";
 import kicad_default from "./kicad-default";
+import custom_kicad from "./custom-kicad";
 
-const themes = [witch_hazel, kicad_default];
+const themes = [witch_hazel, kicad_default, custom_kicad];
 const themes_by_name = new Map(
     themes.map((v) => {
         return [v.name, v];
@@ -16,7 +17,7 @@ const themes_by_name = new Map(
 );
 
 export default {
-    default: witch_hazel,
+    default: custom_kicad,  // <-- make it the default
 
     by_name(name: string): Theme {
         return themes_by_name.get(name) ?? this.default;
